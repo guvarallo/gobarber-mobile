41bc1192f09b1c7c22885843b09041ffdd14615a
guvarallo <gustavo.varallo@live.com>
2020-09-14 19:43:15 -0300

    added signin and signup routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { View, StatusBar } from 'react-native';
 
+import Routes from './routes/index';
+
 const App: React.FC = () => (
   <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Routes />
+    </View>
   </NavigationContainer>
 );
 
