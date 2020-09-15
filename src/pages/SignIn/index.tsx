528c64a13f954e15038a84d376fc52aa7ad59709
guvarallo <gustavo.varallo@live.com>
2020-09-15 11:49:50 -0300

    sign in page styles

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,24 +1,60 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logo from '../../assets/logo.png';
 import Input from '../../components/Input/index';
 import Button from '../../components/Button/index';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logo} />
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Image source={logo} />
 
-      <Title>Sign in below</Title>
+          <View>
+            <Title>Sign in below</Title>
+          </View>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Password" />
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Password" />
 
-      <Button onPress={() => console.log('oi')}>Enter</Button>
-    </Container>
+          <Button onPress={() => console.log('button')}>Enter</Button>
+
+          <ForgotPassword onPress={() => console.log('forgot')}>
+            <ForgotPasswordText>Forgot my password</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </ScrollView>
+
+      <CreateAccountButton onPress={() => console.log('account')}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Create a new account</CreateAccountButtonText>
+      </CreateAccountButton>
+    </KeyboardAvoidingView>
   );
 };
 
